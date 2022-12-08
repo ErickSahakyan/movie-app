d411a5f12316a28b19daccb7a83ee20590755590
ErickSahakyan <erik-saakyan-2000@mail.ru>
2022-12-09 01:00:34 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -11,14 +11,11 @@ function App() {
 	const [movies, setMovies] = useState([]);
 	const [searchValue, setSearchValue] = useState('');
 	const [favorites, setFavorites] = useState([]);
-	
+
 	const getMovieRequest = async (searchValue) => {
 		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
 
-		const response = await fetch(url, {
-			method: "GET",
-			mode: 'cors',
-		});
+		const response = await fetch(url);
 		const responseJson = await response.json();
 
 		if (responseJson.Search) {

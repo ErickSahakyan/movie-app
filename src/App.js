79bd2ae9095ffb7eeabd81762492237f0b88fc22
ErickSahakyan <erik-saakyan-2000@mail.ru>
2022-12-09 17:03:45 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -7,25 +7,24 @@ import SearchBox from './components/SearchBox';
 import AddFavorite from './components/AddFavorite';
 import RemoveFavorite from './components/RemoveFavorite';
 
+
 function App() {
 	const [movies, setMovies] = useState([]);
 	const [searchValue, setSearchValue] = useState('');
 	const [favorites, setFavorites] = useState([]);
 
-	async function getMovieRequest(searchValue) {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
 
-		const response = await fetch(url, {
-			method: 'GET',
-			headers: {
-				"Content-Type": "text/plain",
-			},
-		}).then((res) => res.json());
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=7248391e`;
 
-		if (response.Search) {
-			setMovies(response.Search);
+		const response = await fetch(url);
+		const responseJson = await response.json();
+
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
 		}
-	}
+
+	};
 
 	// const getMovieRequest = async (searchValue) => {
 	// 	const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;

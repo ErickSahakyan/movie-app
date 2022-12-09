630bad7c7bc320b048c57bf2930ca9e8a36db191
ErickSahakyan <erik-saakyan-2000@mail.ru>
2022-12-09 17:41:08 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=7248391e`;
 
 		const response = await fetch(url, {
-			mode: 'cors'
+			mode: 'cors',
 		});
 		const responseJson = await response.json();
 
